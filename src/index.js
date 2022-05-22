375955617df0460d8f8a23b08a0811bfbfca3432
minami <48480944+norizone@users.noreply.github.com>
2022-05-22 18:25:12 +0900

    todoo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import "./styles.css";
-console.log("hoge");
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const addButton = document.getElementById("add_button");
+const addList = document.getElementById("add_todo");
+
+addButton.addEventListener("click", () => {
+  let addText = addList.value || null;
+  if (addText) {
+    const todo = new TodoList(addText);
+    todo.init();
+    addList.value = "";
+  }
+});
+
+class TodoList {
+  constructor(todoText) {
+    this.parent = null;
+    this.todoText = todoText;
+    this.list = document.createElement("li");
+    this.finBtn = document.createElement("button");
+    this.removeBtn = document.createElement("button");
+    this.returnBtn = document.createElement("button");
+  }
+
+  init() {
+    this.parent = document.getElementById("todo_wrap");
+    this.list.innerText = this.todoText;
+    this.parent.appendChild(this.list);
+    this.finBtn.innerText = "完了";
+    this.removeBtn.innerText = "削除";
+    this.list.appendChild(this.finBtn);
+    this.list.appendChild(this.removeBtn);
+    this.removeTodo();
+    this.finTodo();
+  }
+
+  removeTodo() {
+    this.removeBtn.addEventListener("click", () => {
+      this.list.remove();
+    });
+  }
+
+  finTodo() {
+    this.finBtn.addEventListener("click", () => {
+      this.list.remove();
+      this.finBtn.remove();
+      this.removeBtn.remove();
+      this.parent = document.getElementById("fin_wrap");
+      this.parent.appendChild(this.list);
+      this.returnBtn.innerText = "戻っす";
+      this.list.appendChild(this.returnBtn);
+      this.returnTodo();
+    });
+  }
+
+  returnTodo() {
+    this.returnBtn.addEventListener("click", () => {
+      this.list.remove();
+      this.init();
+    });
+  }
+}
